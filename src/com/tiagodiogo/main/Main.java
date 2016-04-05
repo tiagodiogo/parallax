49af7adb299036e94dbab35f1427c41fa4ec75df
Tiago Diogo <tiago.diogo@ist.utl.pt>
2016-04-05 02:40:31 +0100

    added web-based bootstraper (node firmware flasher)

--- src/com/tiagodiogo/main/Main.java
+++ src/com/tiagodiogo/main/Main.java
@@ -1,12 +1,19 @@
 package com.tiagodiogo.main;
 
+import org.eclipse.californium.core.CoapClient;
+
 import com.tiagodiogo.examples.ClientExample;
 
 public class Main {
 
 	public static void main(String [] args)
 	{
-		ClientExample.main(args);
+		//ClientExample.main(args);
+		
+		CoapClient client = new CoapClient("coap://californium.eclipse.org:5683/obs");
+		String response = client.get().getResponseText();
+		System.out.print(response);
+		
 		
 	}
 	
---
+++ web/flash.php
@@ -0,0 +1,30 @@
+<?php
+
+  $command1 = "sudo make clean -C";
+  $command2 = "sudo make -C";
+  $path = " /home/tiago/git/6lbr-develop/examples/6lbr-demo TARGET=";
+
+  $coap="WITH_COAPSERVER=1 ";
+  $llsec="WITH_LLSEC=1 ";
+  $dtls="WITH_TINYDTLS=1 WITH_DTLS_COAP=1";
+
+  $command1 = $command1.$path;
+  $command2 = $command2.$path;
+
+  $device = escapeshellarg($_POST['device']);
+  $command1 = $command1.$device." ";
+  $command2 = $command2.$device." ";
+
+  if($_POST['coap'] == "true"){$command2 = $command2.$coap;}
+  if($_POST['llsec'] == "true"){$command2 = $command2.$llsec;}
+  if($_POST['dtls'] == "true"){$command2 = $command2.$dtls;}
+
+  $command2 = $command2." 6lbr-demo.upload";
+
+  exec("/home/tiago/workspace/parallax/scripts/flash.sh '$command1'");
+  exec("/home/tiago/workspace/parallax/scripts/flash.sh '$command2'");
+
+  #echo shell_exec("/home/tiago/workspace/parallax/scripts/flash.sh '$command1'");
+  #echo shell_exec("/home/tiago/workspace/parallax/scripts/flash.sh '$command2'");
+
+?>
