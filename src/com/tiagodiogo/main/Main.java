b11d39071ca2fddf218ca53b51ff74914accdbbc
Tiago Diogo <tiago.diogo@ist.utl.pt>
2016-04-12 20:34:23 +0100

    created successful connection to mysql database (C3PO)

--- src/com/tiagodiogo/main/Main.java
+++ src/com/tiagodiogo/main/Main.java
@@ -1,13 +1,18 @@
 package com.tiagodiogo.main;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map.Entry;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 import org.eclipse.californium.core.CoapClient;
 
-import com.tiagodiogo.examples.ClientExample;
 import com.tiagodiogo.utils.BorderRouterCrawler;
 
 public class Main {
@@ -15,15 +20,51 @@ public class Main {
 	public static void main(String [] args) throws IOException
 	{
 	
-		HashMap<String, String> nodes = BorderRouterCrawler.parse("http://www.tiagodiogo.com/iot/sensors.html");
+		HashMap<String, String> nodes = BorderRouterCrawler.parse("http://[bbbb::100]/sensors.html");
+		String response = null;
 		for(Entry<String, String> entry : nodes.entrySet()){
 			System.out.println("Node: " + entry.getKey() + " -> Parent: " + entry.getValue());
+			CoapClient client = new CoapClient("coap://["+entry.getKey()+"]:5683/cfg/stack/mac");
+			response = client.get().getResponseText();
+			System.out.print(response);
 		}
 		
-		/*CoapClient client = new CoapClient("coap://californium.eclipse.org:5683/obs");
-		String response = client.get().getResponseText();
-		System.out.print(response);*/
+		InputStream inputStream;
+		Properties prop = new Properties();
+	
+		inputStream = new FileInputStream("database.properties");
+		prop.load(inputStream);
+   
+		Connection dbConnection = null;
 		
+		try {
+			Class.forName(prop.getProperty("DRIVER"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			dbConnection = DriverManager.getConnection(prop.getProperty("URL"),
+													   prop.getProperty("USER"),
+													   prop.getProperty("PASS"));
+		
+		PreparedStatement preparedStatement = null;
+		String updateTableSQL = "UPDATE authorized_nodes SET active = ? "
+                + " WHERE name = ?";
+		
+		preparedStatement = dbConnection.prepareStatement(updateTableSQL);
+
+		preparedStatement.setBoolean(1, true);
+		preparedStatement.setString(2, response);
+		
+		preparedStatement.executeUpdate();
+
+		preparedStatement.close();
+		dbConnection.close();
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
